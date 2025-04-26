41f600ba78bc6c44289d8397f470a01035903faa
satorutoji <ezdummyacc@gmail.com>
2025-04-26 19:31:17 +0500

    ...

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ document.querySelectorAll('.anchor_link').forEach((element) => {
         scrollToSection('user_data')
     })
 })
+
+const header = document.getElementById('header')
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY
+    header.style.background = `rgba(0, 0, 0, ${Math.min(scrollY / 100, 1)}`
+})
